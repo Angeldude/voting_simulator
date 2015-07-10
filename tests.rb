bf37ff58bc62134c308b5a11cf352d5288387044
Angel Vanegas <angelman@gmail.com>
2015-07-10 13:53:59 -0400

    blargh

--- tests.rb
+++ tests.rb
@@ -5,28 +5,30 @@ def test_campaign
   candidates = [
     Candidate.new("Harold Far", "Republican"),
     Candidate.new("Fara Horde", "Democrat"),
-    Candidate.new("Billy", "Progressive"),
+    Candidate.new("Billy", "Republican"),
     Candidate.new("Frar", "Republican"),
-    Candidate.new("Cara", "Libertarian"),
-    Candidate.new("Lourdes", "Massachusetts Democrat")
+    Candidate.new("Cara", "Democrat"),
+    Candidate.new("Lourdes", "Democrat")
   ]
 
   voters= [
-    Voter.new("Jona", "Libertarian"),
+    Voter.new("Jona", "Massachusetts Progressive"),
     Voter.new("Sally", "Progressive"),
     Voter.new("Billy", "Progressive"),
-    Voter.new("Frar", "Conservative"),
+    Voter.new("Frar", "Progressive"),
     Voter.new("Cara", "Libertarian"),
     Voter.new("Lourdes", "Massachusetts Democrat")
   ]
 
   campaign = Campaign.new(candidates, voters)
 
-  p campaign
-
   candidates.each do |candi|
     voters.each do |voter|
-      campaign.stump_speech(candi, voter)
+      if campaign.stump_speech(candi, voter) and not voter.convinced?
+        voter.convinced = true
+      elsif voter.convinced?
+        "I already voted"
+      end
     end
     p [candi.name, candi.counter]
   end
